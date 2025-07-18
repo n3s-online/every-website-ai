601a3cddb3effad2ea04b82315addf3c2a3f8c0b
n3s-online <n3s.online@gmail.com>
2025-07-18 08:45:57 -0600

    style

--- src/app/[slug]/layout.tsx
+++ src/app/[slug]/layout.tsx
@@ -4,7 +4,7 @@ export default function SlugLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div style={{ width: '100%', height: '100vh', overflow: 'auto' }}>
+    <div style={{ width: "100%", height: "100vh", overflow: "auto" }}>
       {children}
     </div>
   );
